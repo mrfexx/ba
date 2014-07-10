1c5eacfcc2b2ea056446825e873720debee200dc
sebastian <sebastian@guehna.de>
2014-07-10 09:02:41 +0200

    Signed-off-by: sebastian <sebastian@guehna.de>

--- Bachelorarbeit/indizieren.m
+++ Bachelorarbeit/indizieren.m
@@ -1,11 +1,28 @@
-function [ output_args ] = indizieren( datei )
+function [ Bild1, Bild_Ausgabe ] = indizieren( datei )
+if datei
 Bild1=imread(datei);
-[Bild_Ausgabe,map] = rgb2ind(Bild1,2);
-%Bild_Ausgabe=rgb2gray(Bild_Ausgabe);
-imagesc(Bild_Ausgabe);
+Bild_Ausgabe=rgb2gray(Bild1);
+[Bild_Ausgabe,map] = gray2ind(Bild_Ausgabe,256);
 colormap(map);
-contour(Bild_Ausgabe);
+
+%Bild_Ausgabe=imresize(Bild_Ausgabe, 0.2);
+%Bildgröße Anpassen
+
+figure(1);
+subplot(2,2,1);
+imagesc(Bild_Ausgabe);
+
+
+
+subplot(2,2,3)
+Bild_Ausgabe=flipdim(Bild_Ausgabe, 1);
+contour(Bild_Ausgabe, 4);
+
+%disp(Bild_Ausgabe(3,2));
+%disp(Bild_Ausgabe);
+%Werte der indizierten Datei
 axis image;
+
 
 end
 
